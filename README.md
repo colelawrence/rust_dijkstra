5e1c45d6c232d4d9fd5dc4d23bff628b62ea1acc
Cole R Lawrence <msgzht@gmail.com>
2017-10-03 11:27:16 -0500

    Add graph files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dijkstra
+
+This is a continuation of the code I have been writing for [rust_learning_notes hashmap practice](https://github.com/colelawrence/rust_learning_notes/tree/master/hm/).
+
+At the moment, we still need to sort the found paths before searching and returning, because at this time, it is not taking weight into account.
+
+The algorithm works if all weights are equal though!
+
+_maybe we should just make artificial edges between real vertices equal to the weight..._
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+pub mod graph;
+use graph::G;
+
 fn main() {
-    println!("Hello, world!");
+    let mut g: G<&str> = G::new();
+
+    let cole = g.add_vertice("Cole");
+    let carlos = g.add_vertice("Carlos");
+    let george = g.add_vertice("George");
+    let abbie = g.add_vertice("Abbie");
+
+    g.add_edge(cole, carlos, 1.0);
+    g.add_edge(carlos, cole, 1.0);
+    g.add_edge(carlos, george, 1.0);
+    g.add_edge(george, abbie, 1.0);
+    g.add_edge(george, carlos, 2.0);
+    g.add_edge(george, cole, 4.0);
+    g.add_edge(george, carlos, 1.0);
+
+    println!("{:?}", g);
+
+    println!("George to Cole: {:?}", g.shortest_path(george, cole));
+    println!("Cole to Abbie: {:?}", g.shortest_path(cole, abbie));
+    println!("Cole to George: {:?}", g.shortest_path(cole, george));
+    println!("Abbie to Carlos: {:?}", g.shortest_path(abbie, carlos));
+    println!("Complete!");
 }
